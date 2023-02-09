a06441f8b3b8afb497115a56394c6aae6b4c1c39
ningkeh <119771079+ningkeh@users.noreply.github.com>
2023-02-09 23:18:24 +0800

    Add files via upload

---
+++ lanzhoudaily_.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from scrapy import FormRequest
+import re
+from newscrapy.items import NewscrapyItem
+from scrapy.spiders import Rule, CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+from newscrapy.tools import dateGen
+from urllib import parse
+
+
+class mySpider(CrawlSpider):
+    name = "lanzhoudaily"
+    newspapers = "兰州日报"
+    allowed_domains = ['lzrb.lzbs.com.cn']
+
+    def start_requests(self):
+        dates = dateGen(self.start, self.end, "%Y%m/%d")
+        template = "http://lzrb.lzbs.com.cn/{date}/node_R01.html"
+        for d in dates:
+            yield FormRequest(template.format(date = d))
+
+    rules = (
+        Rule(LinkExtractor(allow=('\d+/\d+/node_R\w+.html'))),
+        Rule(LinkExtractor(allow=('content/\d+/\d+/content_\w+.html')), callback="parse_item")
+    )
+
+    def parse_item(self, response):
+        try:
+            title1 = response.xpath("//*[@id='PreTitle']").xpath('string(.)').get()
+            title2 = response.xpath("//*[@id='Title']").xpath('string(.)').get()
+            title = title1 + ' ' + title2
+            content = response.xpath("//founder-content").xpath('string(.)').get()
+            url = response.url
+            date = re.search("content/(\d+/\d+)/", url).group(1)
+            date = '-'.join([date[0:4], date[4:6], date[7:9]])
+            imgs = response.xpath("//div[@class='attachment']//img/@src").getall()
+            imgs = [parse.urljoin(url, imgurl) for imgurl in imgs]
+            html = response.text
+        except Exception as e:
+            return
+        
+        item = NewscrapyItem()
+        item['title'] = title
+        item['content'] = content
+        item['date'] = date
+        item['imgs'] = imgs
+        item['url'] = response.url
+        item['newspaper'] = self.newspapers
+        item['html'] = html
+        yield item
---
+++ niudubao_.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from scrapy import FormRequest
+import re
+from newscrapy.items import NewscrapyItem
+from scrapy.spiders import Rule, CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+from newscrapy.tools import dateGen
+from urllib import parse
+
+
+class mySpider(CrawlSpider):
+    name = "niedubao"
+    newspapers = "镍都报"
+    allowed_domains = ['ndb.jnmc.com']
+
+    def start_requests(self):
+        dates = dateGen(self.start, self.end, "%Y%m/%d")
+        template = "http://ndb.jnmc.com/pc/layout/{date}/node_01.html"
+        for d in dates:
+            yield FormRequest(template.format(date = d))
+
+    rules = (
+        Rule(LinkExtractor(allow=('layout/\d+/\d+/node_\w+.html'))),
+        Rule(LinkExtractor(allow=('con/\d+/\d+/content_\w+.html')), callback="parse_item")
+    )
+
+    def parse_item(self, response):
+        try:
+            title1 = response.xpath("//*[@id='PreTitle']").xpath('string(.)').get()
+            title2 = response.xpath("//*[@id='Title']").xpath('string(.)').get()
+            title = title1 + ' ' + title2
+            content = response.xpath("//founder-content").xpath('string(.)').get()
+            url = response.url
+            date = re.search("con/(\d+/\d+)/", url).group(1)
+            date = '-'.join([date[0:4], date[4:6], date[7:9]])
+            imgs = response.xpath("//div[@class='attachment']//img/@src").getall()
+            imgs = [parse.urljoin(url, imgurl) for imgurl in imgs]
+            html = response.text
+        except Exception as e:
+            return
+        
+        item = NewscrapyItem()
+        item['title'] = title
+        item['content'] = content
+        item['date'] = date
+        item['imgs'] = imgs
+        item['url'] = response.url
+        item['newspaper'] = self.newspapers
+        item['html'] = html
+        yield item

fb20ad3863df06c48577b49ecb8eede5faf1ccd5
victorzhuang21 <119149508+victorzhuang21@users.noreply.github.com>
2023-02-21 22:54:35 +0800

    Add files via upload

---
+++ daliandaily.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from scrapy import FormRequest
+import re
+from newscrapy.items import NewscrapyItem
+from scrapy.spiders import Rule, CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+from newscrapy.tools import dateGen
+from urllib import parse
+
+
+class mySpider(CrawlSpider):
+    name = "daliandaily"
+    newspapers = "大连日报"
+    allowed_domains = ['szb.dlxww.com']
+
+    def start_requests(self):
+        dates = dateGen(self.start, self.end, "%Y%m/%d")
+        template = "http://szb.dlxww.com/epaper/dlrb/pc/layout/202302/20/node_A01.html"
+        for d in dates:
+            yield FormRequest(template.format(date = d))
+
+    rules = (
+        Rule(LinkExtractor(allow=('layout/\d+/\d+/node\w+.htm'))),
+        Rule(LinkExtractor(allow=('content/\d+/\d+/content\w+.htm')), callback="parse_item")
+    )
+
+    def parse_item(self, response):
+        try:
+            title = response.xpath("//div[@class='newsdetatit']//h3").xpath("string(.)").get()
+            content = response.xpath("//founder-content").xpath("string(.)").getall()
+            url = response.url
+            date = re.search("content/(\d+/\d+)/", url).group(1)
+            date = '-'.join([date[0:4], date[4:6], date[7:9]])
+            imgs = response.xpath("//div[@class='pic']//img/@src").getall()
+            imgs = [parse.urljoin(url, imgurl) for imgurl in imgs]
+            html = response.text
+        except Exception as e:
+            return
+
+        item = NewscrapyItem()
+        item['title'] = title
+        item['content'] = content
+        item['date'] = date
+        item['imgs'] = imgs
+        item['url'] = response.url
+        item['newspaper'] = self.newspapers
+        item['html'] = html
+        yield item
